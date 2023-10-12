9566310283a058b39802f0402e942d6cebacd039
subhashn <subhashn@10.18.251.124>
2023-10-12 10:56:10 +0530

    Sonarcube2

--- src/main/java/abc.java
+++ src/main/java/abc.java
@@ -3,7 +3,7 @@ public class abc {
 	public String b;
 	public static void main(String args[]){
 		public String a;
-		System.out.println(a);
+		System.out.println(a):
 		System.out.println("hiiii");
 
 	}
